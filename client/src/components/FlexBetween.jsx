202716e84af0cec7bf4c908cf5dda55b742a67e6
DhruvShah <dhruvshah78223@gmail.com>
2024-12-26 21:27:08 +0530

    Nabar code added.

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+const { styled } = require("@mui/material");
+
+const FlexBetween = styled(Box)({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
+});
+
+export default FlexBetween;
