b21b9ff1de461e2fff6983c157ab026eafa208ff
Your Name <dhruv.shah@ascendion.com>
2025-01-03 12:07:40 +0530

    id will be changed.

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     mode: "dark",
-    userId:"76728787787827ew"
+    userId:"63701cc1f03239c72c00017f"
 };
 
 export const globalSlice = createSlice({
